d47149cd743a7b3565dc8c052b032aa4e8650e6a
artoriaskillerxdie <g.ahss1980@gail.com>
2020-02-29 19:55:00 -0300

    Mudanças do Arthur, entrada padrão

---
+++ filosofia.js
@@ -0,0 +1,31 @@
+function definir_topo_imagem(x){
+    x.style.top = (document.body.clientHeight * 0.1) +'px';
+  }
+ 
+   function definir_tamanho_topo(x){
+    x.style.width = (document.body.clientWidth * 1.2) + 'px';
+
+   } 
+   function definir_tamanho_entrada(x){
+    x.style.width = (document.body.clientWidth * 1.15) + 'px';
+    x.style.top   = (document.body.clientWidth * -0.02) + 'px';
+   }
+   
+   function mudar_tamanho_topo(x){
+     x.style.width =(document.body.clientWidth *1.25) +'px';
+   } 
+    function mudar_tamanho_entrada(x){
+     x.style.width =(document.body.clientWidth *1.15) +'px';
+     x.style.top   = (document.body.clientHeight * -0.02) + 'px';
+   }
+   
+   function definir_esquerda_entrada(x){
+   x.style.left = (document.body.clientWidth * 0.06) + 'px';
+   }
+
+   function definir_esquerda_texto(x){
+    x.style.left = (document.body.clientWidth * 0.6) + 'px';
+    }
+    
+  
+  
