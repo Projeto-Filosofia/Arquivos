7098ce0bcda7c5731754114375a68b29d6e3a8b2
artoriaskillerxdie <g.ahss1980@gail.com>
2020-03-01 19:38:49 -0300

    entrada quase completa, ainda falta terminar o menu

--- filosofia.js
+++ filosofia.js
@@ -4,28 +4,23 @@ function definir_topo_imagem(x){
  
    function definir_tamanho_topo(x){
     x.style.width = (document.body.clientWidth * 1.2) + 'px';
-
+    x.style.left = (document.body.clientWidth * -0.01) + 'px';
+    x.style.top = (document.body.clientWidth * -0.02) + 'px';
    } 
    function definir_tamanho_entrada(x){
-    x.style.width = (document.body.clientWidth * 1.15) + 'px';
+    x.style.width = (document.body.clientWidth * 1.1) + 'px';
     x.style.top   = (document.body.clientWidth * -0.02) + 'px';
    }
-   
-   function mudar_tamanho_topo(x){
-     x.style.width =(document.body.clientWidth *1.25) +'px';
-   } 
-    function mudar_tamanho_entrada(x){
-     x.style.width =(document.body.clientWidth *1.15) +'px';
-     x.style.top   = (document.body.clientHeight * -0.02) + 'px';
-   }
-   
    function definir_esquerda_entrada(x){
    x.style.left = (document.body.clientWidth * 0.06) + 'px';
    }
-
    function definir_esquerda_texto(x){
     x.style.left = (document.body.clientWidth * 0.6) + 'px';
     }
+   function definir_esquerda_menu(x){
+    x.style.left = (document.body.clientWidth * 0.65) + 'px';
+    }
+
     
   
   
